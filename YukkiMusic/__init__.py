c1eb8bb68a1a2a4f28b5e72cc8fc9dbee2192e79
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-05-05 20:40:56 +0300

    Update __init__.py

--- YukkiMusic/__init__.py
+++ YukkiMusic/__init__.py
@@ -1,13 +1,14 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2023 by ArchBots@Github, < https://github.com/ArchBots >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/ArchBots/ArchMusic > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/ArchBots/ArchMusic/blob/master/LICENSE >
 #
 # All rights reserved.
+#
 
-from YukkiMusic.core.bot import YukkiBot
+from YukkiMusic.core.bot import YukkiMusic
 from YukkiMusic.core.dir import dirr
 from YukkiMusic.core.git import git
 from YukkiMusic.core.userbot import Userbot
