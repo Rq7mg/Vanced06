4a76d2e68a3f266779738f59f17086c5fc4712ec
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-05-06 07:31:55 +0300

    Update call.py

--- YukkiMusic/core/call.py
+++ YukkiMusic/core/call.py
@@ -1,18 +1,9 @@
-#
-# Copyright (C) 2021-2023 by ArchBots@Github, < https://github.com/ArchBots >.
-#
-# This file is part of < https://github.com/ArchBots/ArchMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/ArchBots/ArchMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 
 import asyncio
 from datetime import datetime, timedelta
 from typing import Union
 
-from pyrogram import Client, YukkiMusic
+from pyrogram import Client
 from pyrogram.errors import (ChatAdminRequired,
                              UserAlreadyParticipant,
                              UserNotParticipant)
