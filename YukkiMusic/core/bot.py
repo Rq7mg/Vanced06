ec2af80c297d97f0c5bdf5851b339163ee216d9c
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-05-06 08:01:37 +0300

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -7,7 +7,7 @@ import config
 
 from ..logging import LOGGER
 
-class YukkiMusic(Client):
+class Yukki(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
